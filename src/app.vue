f631373ede3820e21fe2e433b8a42a3fa0b13275
Jesswin Chetnani <jesswin.chetnani@hypd.in>
2023-12-07 17:27:07 +0530

    Observation: routes, layout understandings

--- src/app.vue
+++ src/app.vue
@@ -1,4 +1,10 @@
 <template>
-  <div>Nuxt App</div>
+  <div>App Vue (can be considered as default layout (if only one layout))</div>
   <NuxtPage />
 </template>
+
+<script setup>
+onMounted(() => {
+  console.log("App mounted");
+});
+</script>
