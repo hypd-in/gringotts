715bd1c4d56dc1d70cfc0bb01a648f23807195dc
Akarshit Kamra <akarshit.kamra@hypd.in>
2023-12-07 14:55:42 +0530

    Added head meta tags, routing and icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,86 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  devServer: {
+    "port": 8080,
+  },
+  srcDir: "src",
+  app: {
+    head: {
+      title: "HYPD Store | #getHYPD",
+      meta: [
+        // Essentials
+        {
+          charset: "utf-8",
+        },
+        {
+          "http-equiv": "X-UA-Compatible",
+          content: "IE=edge",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1",
+        },
+        {
+          property: "og:image",
+          content: "https://www.hypd.store/img/social-logo.png",
+        },
+
+        // Twitter
+        {
+          name: "twitter:card",
+          content: "summary",
+        },
+        {
+          name: "twitter:image",
+          content: "https://www.hypd.store/img/social-logo.png",
+        },
+        {
+          name: "theme-color",
+          content: "#342546",
+        },
+
+        // Page Description
+        {
+          name: "description",
+          content:
+            "Shop from your favourite Creator's recommendations directly from their store! | #ItsAFullTimeJob | #getHYPD",
+        },
+        {
+          property: "og:description",
+          content:
+            "Shop from your favourite Creator's recommendations directly from their store! | #ItsAFullTimeJob | #getHYPD",
+        },
+        {
+          property: "og:title",
+          content:
+            "Shop from your favourite Creator's recommendations directly from their store! | #ItsAFullTimeJob | #getHYPD",
+        },
+        // Google Ads
+        {
+          property: "og:locale",
+          content: "en_IN",
+        },
+        {
+          name: "google-site-verification",
+          content: "W6snttufB8sOVQbc9Hem5pmET3utO_enRiZWtO9lHIw",
+        },
+        {
+          name: "google-site-verification",
+          content: "eLJFdksmhTV_HZe5_bAY1-LuE0oUWKMhOnBwxoVMq9Q",
+        },
+      ],
+      link: [
+        {
+          rel: "canonical",
+          href: "https://www.hypd.store",
+        },
+      ],
+    },
+  },
+});
---
+++ src/app.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>Nuxt App</div>
+  <NuxtPage />
+</template>
