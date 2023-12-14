e92b5eab7c6d9a0505b70c9914d115e6ddd88619
Sushant Kumar <sushant.kumar@hypd.in>
2023-12-14 16:47:53 +0530

    meta info and api call

--- src/pages/[creatorUsername]/brand/[brandUsername].vue
+++ src/pages/[creatorUsername]/brand/[brandUsername].vue
@@ -1,51 +1,45 @@
 <template>
   {{ route.params }}
-  {{ response.data.payload }}
+  {{ brandInfo.payload }}
 </template>
 
 <script setup>
+const nuxtApp = useNuxtApp();
+console.log("fuck yay", nuxtApp.payload?.data?.brand_profile_info, "HELP FUCK");
+
 import { getBrandInfoFromUsername } from "../../../API/brandProfile.js";
-import { useRoute } from "vue-router";
-
-const response = ref(null);
-console.log("HERE");
-response.value = await useFetch(
-  `https://entity.getshitdone.in/api/app/brand/username/ghumakkad`,
-  {
-    onResponse({ request, response, options }) {
-      // Process the response data
-      console.log( request, response, options);
-      useSeoMeta({
-        description: () => response?._data?.payload?.bio,
-        ogTitle: () => response?._data?.payload?.name,
-        ogDescription: () => response?._data?.payload?.bio,
-        ogImage: () => response?._data?.payload?.cover_img?.src,
-        ogUrl: () => response?._data?.payload?.website,
-        twitterTitle: () => response?._data?.payload?.name,
-        twitterDescription: () => response?._data?.payload?.bio,
-        twitterImage: () => response?._data?.payload?.cover_img?.src,
-        twitterCard: "summary",
-      });
-
-      useHead({
-        htmlAttrs: {
-          lang: "en",
-        },
-        title: () => response?.data?.payload?.name,
-        link: [
-          {
-            rel: "icon",
-            type: "image/png",
-            href: "/favicon.png",
-          },
-        ],
-      });
-    },
-  }
-);
 
 const route = useRoute();
-console.log(route.params); // { id: '123' }
+const brandUsername = route.params.brandUsername;
+const uri =
+  "https://entity.getshitdone.in/api/app/brand/username/" + brandUsername;
+
+// const { data: brandInfo, pending, error, refresh } = await useAsyncData(
+//   'mountains',
+//   () => $fetch(uri)
+// )
+const { data: brandInfo } = await useFetch(uri, {
+  key: "brand_profile_info",
+  pick: ["payload"],
+  onResponse({ request, response, options }) {
+    // Process the response data
+    console.log(response._data.payload);
+  },
+});
+
+
+useSeoMeta({
+  ogtitle: () => brandInfo.value.name,
+  description: () => brandInfo.value.payload.bio,
+  ogImage: () => brandInfo.value.payload.cover_img.src,
+  ogDescription: () => brandInfo.value.payload.bio,
+  ogImage: () => brandInfo.value.payload?.cover_img?.src,
+  ogUrl: () => brandInfo.value.payload?.website,
+  twitterTitle: () => brandInfo.value.payload?.name,
+  twitterDescription: () => brandInfo.value.payload?.bio,
+  twitterImage: () => brandInfo.value.payload?.cover_img?.src,
+  twitterCard: "summary",
+});
 </script>
 
 <style>
