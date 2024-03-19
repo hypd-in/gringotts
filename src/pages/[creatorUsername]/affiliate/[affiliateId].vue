20d47ca977ac4ee1d6057a7a814c9aca49f57641
akarshitkamra-hypd <akarshit.kamra@hypd.in>
2024-03-19 13:54:58 +0530

    Added redirection

---
+++ src/pages/[creatorUsername]/affiliate/[affiliateId].vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "standalone",
+})
+const route = useRoute();
+onMounted(() => {
+  if (route.params.creatorUsername && route.params.affiliateId) {
+    navigateTo({
+      name: "AfflinkRedirection",
+      params: {
+        creatorUsername: route.params.creatorUsername,
+        afflinkId: route.params.affiliateId,
+      },
+    })
+  }
+})
+</script>
